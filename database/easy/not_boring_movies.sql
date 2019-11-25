102d4f1be0273ef3a766f69dcd252313ef18ad74
Latofat Bobojonova <latofatbobojonova@gmail.com>
2019-11-25 11:08:10 +0500

    Add other solutions to Not Boring Movies

--- database/easy/not_boring_movies.sql
+++ database/easy/not_boring_movies.sql
@@ -4,4 +4,17 @@ SELECT *
 FROM cinema
 WHERE id % 2 = 1
   AND NOT description = 'boring'
-ORDER BY rating DESC
+ORDER BY rating DESC;
+
+SELECT *
+FROM cinema
+WHERE mod(id, 2) = 1
+  AND description != 'boring'
+ORDER BY rating DESC;
+
+SELECT *
+FROM cinema
+WHERE mod(id, 2) = 1
+  AND description <> 'boring'
+ORDER BY rating DESC;
+
