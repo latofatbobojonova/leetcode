319fd7f5276f0f4aa9e5c8215135d7cece2f9461
Latofat Bobojonova <latofatbobojonova@gmail.com>
2019-11-25 14:51:33 +0500

    Add solution Reverse Integer

---
+++ algorithms/easy/ReverseInteger.java
@@ -0,0 +1,26 @@
+package easy;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        if (x == 0) {
+            return 0;
+        }
+
+        while (x % 10 == 0) {
+            x /= 10;
+        }
+
+        long reversed = 0;
+
+        while (x != 0) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        if (reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
+            return 0;
+        }
+
+        return (int) reversed;
+    }
+}
