6cb3f87d325541dd6c44522a5a567fcf38218946
Latofat Bobojonova <latofatbobojonova@gmail.com>
2019-11-25 11:00:40 +0500

    Add solution Rising Temperature

--- database/easy/rising_temperature.sql
+++ database/easy/rising_temperature.sql
@@ -4,4 +4,4 @@ SELECT t1.id
 FROM weather t1
          INNER JOIN
      weather t2 ON DATE_SUB(t1.recorddate, INTERVAL 1 DAY) = t2.recorddate
-WHERE t1.temperature > t2.temperature
+WHERE t1.temperature > t2.temperature;
