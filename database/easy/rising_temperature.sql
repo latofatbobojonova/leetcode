59cf8c25cfbce85fcf13c2d6985b64b486482987
Latofat Bobojonova <latofatbobojonova@gmail.com>
2019-11-25 10:59:41 +0500

    Add solution Customer Who Never Order

---
+++ database/easy/rising_temperature.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/rising-temperature/
+
+SELECT t1.id
+FROM weather t1
+         INNER JOIN
+     weather t2 ON DATE_SUB(t1.recorddate, INTERVAL 1 DAY) = t2.recorddate
+WHERE t1.temperature > t2.temperature
