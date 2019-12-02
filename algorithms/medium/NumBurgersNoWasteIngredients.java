b08b1431fc834e627734ec994cb8052faa5b5e79
Latofat Bobojonova <latofatbobojonova@gmail.com>
2019-12-02 15:53:57 +0500

    Add brute-force solution Number of Burgers with No Waste of Ingredients

---
+++ algorithms/medium/NumBurgersNoWasteIngredients.java
@@ -0,0 +1,38 @@
+//https://leetcode.com/problems/number-of-burgers-with-no-waste-of-ingredients/
+
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumBurgersNoWasteIngredients {
+    public List<Integer> numOfBurgers(int tomatoSlices, int cheeseSlices) {
+        int totalJumbo = 0;
+        int totalSmall = 0;
+        List<Integer> burgerNum = new ArrayList<>();
+
+        if (tomatoSlices % 2 != 0)
+            return burgerNum;
+
+        if (tomatoSlices < cheeseSlices)
+            return burgerNum;
+
+        int remainder = tomatoSlices % 4;
+        int k = tomatoSlices / 4;
+
+        if (!(cheeseSlices >= k + remainder / 2 && cheeseSlices <= 2 * k + remainder / 2))
+            return burgerNum;
+
+        for (int i = 0; i <= cheeseSlices; i++) {
+            if (2 * i + 4 * (cheeseSlices - i) == tomatoSlices) {
+                totalSmall = i;
+                totalJumbo = cheeseSlices - i;
+                break;
+            }
+        }
+        burgerNum.add(totalJumbo);
+        burgerNum.add(totalSmall);
+
+        return burgerNum;
+    }
+}
