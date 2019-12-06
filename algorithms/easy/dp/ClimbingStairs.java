ea6000bccaaae8c4fac01925b358c5c089cce4ca
Latofat Bobojonova <latofatbobojonova@gmail.com>
2019-12-06 12:09:22 +0500

    Add solution Climbing Stairs

---
+++ algorithms/easy/dp/ClimbingStairs.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/climbing-stairs/
+
+package easy.dp;
+
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        int dp = 1;
+        int dp1 = 0;
+
+        for (int i = 0; i <= n; i++) {
+            dp1 = dp1 + dp;
+            dp = dp1 - dp;
+        }
+        return dp1;
+    }
+}
