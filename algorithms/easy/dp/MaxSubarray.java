5edee2d7e9e4b55ba2a13c77eb5c78f97dee596f
Latofat Bobojonova <latofatbobojonova@gmail.com>
2019-12-06 15:18:51 +0500

    Add solution Max Subarray

---
+++ algorithms/easy/dp/MaxSubarray.java
@@ -0,0 +1,19 @@
+package easy.dp;
+
+public class MaxSubarray {
+    public int maxSubArray(int[] nums) {
+        int len = nums.length;
+
+        int prevMax = nums[0];
+        int overAllMax = nums[0];
+
+        for (int i = 1; i < len; i++) {
+            // Max sum of subarray whose last index is i
+            prevMax = (prevMax + nums[i] > nums[i]) ? prevMax + nums[i] : nums[i];
+
+            overAllMax = (overAllMax < prevMax) ? prevMax : overAllMax;
+        }
+
+        return overAllMax;
+    }
+}
