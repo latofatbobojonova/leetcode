6db34eda2006796b3640b93dfee0d94e36476c80
Latofat Bobojonova <latofatbobojonova@gmail.com>
2019-12-07 09:37:49 +0500

    Add solution House Robber

---
+++ algorithms/easy/dp/HouseRobber.java
@@ -0,0 +1,32 @@
+package easy.dp;
+
+public class HouseRobber {
+    public int rob(int[] nums) {
+        int len = nums.length;
+        int[] dp = new int[len];
+
+        if (len == 0) {
+            return 0;
+        }
+
+        if (len == 1) {
+            return nums[0];
+        }
+
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+
+        if (len > 2)
+            dp[2] = nums[0] + nums[2];
+
+        for (int i = 3; i < len; i++) {
+            dp[i] = max(dp[i - 2], dp[i - 3]) + nums[i];
+        }
+
+        return max(dp[len - 2], dp[len - 1]);
+    }
+
+    public int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+}
