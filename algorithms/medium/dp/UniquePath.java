abacddc5147bce3451d711ba5735153d45fb930a
Latofat Bobojonova <latofatbobojonova@gmail.com>
2019-12-09 15:02:11 +0500

    Add solution Unique Path

---
+++ algorithms/medium/dp/UniquePath.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/unique-paths/
+package medium.dp;
+
+public class UniquePath {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        if (m == 0 || n == 0)
+            return 0;
+
+        for (int i = 0; i < m; i++)
+            dp[i][0] = 1;
+
+        for (int j = 0; j < n; j++)
+            dp[0][j] = 1;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
