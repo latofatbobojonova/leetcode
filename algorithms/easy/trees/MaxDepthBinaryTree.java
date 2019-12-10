af7ad3345b0c79d66e9cdabedee540ee678eccca
Latofat Bobojonova <latofatbobojonova@gmail.com>
2019-12-10 11:15:54 +0500

    Remove main method

--- algorithms/easy/trees/MaxDepthBinaryTree.java
+++ algorithms/easy/trees/MaxDepthBinaryTree.java
@@ -4,16 +4,6 @@ package easy.trees;
 import tree.TreeNode;
 
 public class MaxDepthBinaryTree {
-    public static void main(String[] args) {
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.left.left = new TreeNode(3);
-        root.left.right = new TreeNode(4);
-        root.right = new TreeNode(5);
-
-        System.out.println(new MaxDepthBinaryTree().maxDepth(root));
-    }
-
     public int maxDepth(TreeNode root) {
         if (root == null)
             return 0;
